af253a2aaf4660cbeaf32cb3365ae2bc33c6ed38
Raiyan Razi <raiyanrazi@ip-192-168-12-24.ap-south-1.compute.internal>
2020-04-04 01:23:33 +0530

    forth

--- first_etl.py
+++ first_etl.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as sf
 from pyspark.sql import types as SparkTypes
+import dateutil.parser as parser
 import uuid
 import psycopg2
 
@@ -8,7 +9,8 @@ mongoFormat = "com.mongodb.spark.sql.DefaultSource"
 mongoUriOption = "spark.mongodb.input.uri"
 mongoCollectionOption = "spark.mongodb.input.collection"
 
-mongoCoreUrl = "mongodb://rpkdbdev:y5MpwjMZv3^z!4eN@rupeek-shard-00-00-knojx.gcp.mongodb.net:27017,rupeek-shard-00-01-knojx.gcp.mongodb.net:27017,rupeek-shard-00-02-knojx.gcp.mongodb.net:27017/rpkdbdev?ssl=true&replicaSet=rupeek-shard-0&authSource=admin"
+# mongoCoreUrl = "mongodb://rpkdbdev:y5MpwjMZv3^z!4eN@rupeek-shard-00-00-knojx.gcp.mongodb.net:27017,rupeek-shard-00-01-knojx.gcp.mongodb.net:27017,rupeek-shard-00-02-knojx.gcp.mongodb.net:27017/rpkdbdev?ssl=true&replicaSet=rupeek-shard-0&authSource=admin"
+mongoCoreUrl = "mongodb://datasrv:CcMn0kBxZ6gV2pod@prod-shard-00-00-5eaed.mongodb.net:27017,prod-shard-00-01-5eaed.mongodb.net:27017,prod-shard-00-02-5eaed.mongodb.net:27017/rpkdb?authSource=admin&replicaSet=prod-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true"
 
 pgUriOption = "url"
 
@@ -34,7 +36,8 @@ def getMostRecentUpdatedAt(dbtable):
     query = '(SELECT max("updatedAt") FROM '+dbtable+') foo'
     pgDF = spark.read.format('jdbc').option("url", pgConnUrl).option("user", pgUser).option(
         "password", pgPassword).option("driver", pgDriver).option("dbtable", query).load()
-    return pgDF.collect()[0]["max"]
+    recentDate = pgDF.collect()[0]["max"]
+    return recentDate.isoformat() if recentDate is not None else parser.parse('1970-01-01 00:00:00.000+00').isoformat()
 
 
 def getMongoUserDataframe(customerRecentUpdatedAt):
@@ -61,7 +64,7 @@ def upsertCustomers(mongoUserDF):
         cursor = conn.cursor()
         conn.commit()
         for row in customersToBeUpserted:
-            upsertQuery = 'INSERT INTO customer_test (id,mongoid,"createdAt","updatedAt") VALUES (\'{id}\',\'{mongoid}\',\'{createdAt}\', \'{updatedAt}\')' \
+            upsertQuery = 'INSERT INTO customers (id,mongoid,"createdAt","updatedAt") VALUES (\'{id}\',\'{mongoid}\',\'{createdAt}\', \'{updatedAt}\')' \
                       ' ON CONFLICT (mongoid) DO UPDATE SET "createdAt" = EXCLUDED."createdAt", "updatedAt" = EXCLUDED."updatedAt"'.format(
                         id=row.id, mongoid=row.mongoid, createdAt=row.createdAt, updatedAt=row.updatedAt)
             cursor.execute(upsertQuery)
@@ -77,7 +80,7 @@ def upsertPhones(mongoUserDF):
     phonesToBeUpsertedDF = mongoUserDF.select(sf.col('phone').alias('primaryPhone'), 'createdAt', 'updatedAt', 'phones', 'mongoid', )
     mongoIDs = [row.mongoid for row in phonesToBeUpsertedDF.collect()]
     mongoIDsString = ("' , '").join(mongoIDs)
-    pgQuery = "(SELECT id, mongoid FROM customer_test WHERE mongoid IN ('"+mongoIDsString+"')) foo"
+    pgQuery = "(SELECT id, mongoid FROM customers WHERE mongoid IN ('"+mongoIDsString+"')) foo"
     pgCustomerDF = getPGDataframe(pgQuery)
     phonesAssociatedCustomerDF = phonesToBeUpsertedDF.join(pgCustomerDF, on=['mongoid'], how='left')
     # The id in the join is customer's id not postGres Phone id
@@ -86,10 +89,10 @@ def upsertPhones(mongoUserDF):
     phones = phones.withColumn('otherPhone', sf.when(sf.col('otherPhone').isNull(), sf.col('primaryPhone')).otherwise(sf.col('otherPhone')))
     phones = phones.withColumn('isPrimary', sf.when(sf.col('otherPhone') == sf.col('primaryPhone'), True).otherwise(False))
     phones = phones.select('customer', 'createdat', 'updatedat', sf.col('otherPhone').alias('phone'), 'isPrimary')
-    pgPhonesDF = getPGDataframe("customerphones_test")
+    pgPhonesDF = getPGDataframe("customerphones")
     phones = phones.filter(~(sf.concat(sf.col('customer'), sf.lit('_'), sf.col('phone')))
                            .isin([str(row.customer)+'_'+str(row.phone) for row in pgPhonesDF.collect()]))
-    writeDaataframetoPG(phones, "customerphones_test")
+    writeDataframetoPG(phones, "customerphones")
 
 
 def getMongoCustomerProfileDataframe(CPrecentUpdatedAt):
@@ -97,42 +100,40 @@ def getMongoCustomerProfileDataframe(CPrecentUpdatedAt):
     pipeline = [{'$match': {"updatedAt": {'$gt': {'$date': CPrecentUpdatedAt}}}}]
     mongoCustomerProfileDF = spark.read.format(mongoFormat)\
         .option(mongoUriOption, mongoCoreUrl)\
-        .option(mongoCollectionOption,mongoCollection)\
+        .option(mongoCollectionOption, mongoCollection)\
         .option("pipeline", pipeline)\
         .load()
-    mongoCustomerProfileDF = mongoCustomerProfileDF.select(sf.col('_id.oid').alias('lenderid'),'user', 'lpinfo', sf.col('createdat').alias('createdAtLender'), sf.col('updatedAt').alias('updatedAtLender'))
+    mongoCustomerProfileDF = mongoCustomerProfileDF.select(sf.col('_id.oid').alias('lenderid'),'user', 'lpinfo', 'createdAt', 'updatedAt')
     mongoCustomerProfileDF.printSchema()
     return mongoCustomerProfileDF
 
-def writeDaataframetoPG(df, pgDB):
+def writeDataframetoPG(df, pgDB):
     df.write.format('jdbc').mode('append').option('url', pgConnUrl).option('user', pgUser)\
         .option('password', pgPassword).option('dbtable', pgDB).option("driver", "org.postgresql.Driver").save()
 
 def upsertLenderCustomer():
-    lenderRecentUpdatedAt = getMostRecentUpdatedAt('lendercustomer_test')
-
+    lenderRecentUpdatedAt = getMostRecentUpdatedAt('lender_customers')
     mongoCustomerProfileDF = getMongoCustomerProfileDataframe(lenderRecentUpdatedAt)
 
-    query = '(SELECT id,mongoid FROM customer_test) foo'
+    query = '(SELECT id,mongoid FROM customers) foo'
     pgCustomerDF = getPGDataframe(query)
-    lenderCustomerDF = mongoCustomerProfileDF.join(pgCustomerDF, pgCustomerDF.mongoid == mongoCustomerProfileDF.user, how='left')
+    pgCustomerDF.printSchema()
+    mongoCustomerProfileDF.printSchema()
+    lenderCustomerDF = mongoCustomerProfileDF.join(pgCustomerDF, pgCustomerDF.mongoid == mongoCustomerProfileDF.user.oid, how='left')
     lenderCustomerDF = lenderCustomerDF.select(sf.col('id').alias('customer'), 'createdat', 'updatedAt',
                                                sf.explode('lpinfo').alias('lpinfo'))
     # lenderCustomerDF = lenderCustomerDF.filter(~sf.col('customer').isNull())
+    lenderCustomerDF.orderBy(sf.desc('customer')).show(20, False)
     lenderCustomerDF = lenderCustomerDF\
         .withColumn('rupeek_ref', lenderCustomerDF.lpinfo.rupeekid)\
-        .withColumn('lender', lenderCustomerDF.lpinfo.name)\
+        .withColumn('lender', sf.when(sf.col('lpinfo.name').isNull(), None).otherwise(sf.col('lpinfo.name')))\
         .withColumn('lender_ref', lenderCustomerDF.lpinfo.id)
 
     lenderCustomerDF = lenderCustomerDF.select('customer', 'createdat', 'updatedat', 'rupeek_ref', 'lender', 'lender_ref')
     lenderCustomerDF = attachIdToDataframe(lenderCustomerDF)
     lenderCustomerDF.printSchema()
-
-    pgLenderCustomerDF = getPGDataframe('lendercustomer_test')
-    # print('------ count -------')
-    # lenderCustomerDF.count()
-    # print('------ count -------')
-    # lenderCustomerDF.show(20, False)
+    lenderCustomerDF.show(200, False)
+    # pgLenderCustomerDF = getPGDataframe('lendercustomers')
     return lenderCustomerDF
 
 
@@ -145,12 +146,11 @@ def attachIdToDataframe(dataframe):
 def main():
     global spark
     spark = SparkSession.builder.appName("Mongo to Postgres Transformation").getOrCreate()
-    customerRecentUpdatedAt = getMostRecentUpdatedAt('customer').isoformat()
-    mongoUserDF = getMongoUserDataframe(customerRecentUpdatedAt)
-    upsertCustomers(mongoUserDF)
-    upsertPhones(mongoUserDF)
-    # upsertLenderCustomer()
-
+    customerRecentUpdatedAt = getMostRecentUpdatedAt('customers')
+    # mongoUserDF = getMongoUserDataframe(customerRecentUpdatedAt)
+    # upsertCustomers(mongoUserDF)
+    # upsertPhones(mongoUserDF)
+    upsertLenderCustomer()
 
 
 if __name__ == '__main__':
